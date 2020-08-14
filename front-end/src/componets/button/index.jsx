78f4e2dd7fa43855bc7708e86b9b55b7c35aaac6
Dagoberto Rojas <krojas96ce@gmail.com>
2020-08-13 20:44:04 -0600

    Gui Login, falta funcionidad de SPA

---
+++ front-end/src/componets/button/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from 'react-bootstrap'
+export default class TECButton extends React.Component{
+    render(){
+        return(
+            <Button className="TECbtn">
+                "TSt btn"
+            </Button>
+        );
+    }
+}
