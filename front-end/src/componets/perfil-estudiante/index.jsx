c80b0f5427531b4aa6ebda74a5ec1fe0664a5d88
Jung <bakkim05@gmail.com>
2020-08-18 02:08:46 -0600

    version estatica

--- front-end/src/componets/perfil-estudiante/index.jsx
+++ front-end/src/componets/perfil-estudiante/index.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col'
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import Navbar from "react-bootstrap/Navbar";
+import Col from 'react-bootstrap/Col';
+import Table from 'react-bootstrap/Table';
+import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
 
 import { setUserPass } from "./../../store/tipo_usuario/action";
 import "./perfil-estudiante.css";
 import logo from "../../img/logo.svg";
+import liam from "../../img/liam.jpg";
 import { NavigationBar } from "../NavigationBar";
 import postUserPassEstudiante from "./../../queries/axios";
 
@@ -38,16 +39,170 @@ class PerfilEstudiante extends React.Component {
           <NavigationBar/>
 
           <Container>
-              <Col>
-                  <p>
-                      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                  </p>
+            <Row>
+              <Col md={{ span: 2}}>
+                <div><Image src={liam} thumbnail /></div>
+                <div><Button variant="link">Cambiar Foto</Button>{' '}</div>
               </Col>
-              <Col>
-                  <p>
-                      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                  </p>
+              <Col md={{ span: 4}}>
+                <div>Información Personal</div>
+                <div>
+                  <Container>
+                    <Form>
+                      <Form.Group as={Row} controlId="formPlaintextEmail">
+                        <Form.Label column sm="4">
+                          Nombre
+                        </Form.Label>
+                        <Col sm="8">
+                          <Form.Control plaintext readOnly defaultValue="Liam" />
+                        </Col>
+                      </Form.Group>
+                      <Form.Group as={Row} controlId="formPlaintextEmail">
+                        <Form.Label column sm="4">
+                          Apellido
+                        </Form.Label>
+                        <Col sm="8">
+                          <Form.Control plaintext readOnly defaultValue="Hemsworth" />
+                        </Col>
+                      </Form.Group>
+                      <Form.Group as={Row} controlId="formPlaintextEmail">
+                        <Form.Label column sm="4">
+                          Carné
+                        </Form.Label>
+                        <Col sm="8">
+                          <Form.Control plaintext readOnly defaultValue="2016193299" />
+                        </Col>
+                      </Form.Group>
+                      <Form.Group as={Row} controlId="formPlaintextEmail">
+                        <Form.Label column sm="4">
+                          Universidad
+                        </Form.Label>
+                        <Col sm="8">
+                          <Form.Control plaintext readOnly defaultValue="X-TEC" />
+                        </Col>
+                      </Form.Group>
+                      <Form.Group as={Row} controlId="formPlaintextEmail">
+                        <Form.Label column sm="4">
+                          Sede
+                        </Form.Label>
+                        <Col sm="8">
+                          <Form.Control plaintext readOnly defaultValue="Cartago" />
+                        </Col>
+                      </Form.Group>
+                    </Form>
+                  </Container>
+                </div>
               </Col>
+              <Col md={{ span: 6}}>
+                <div>Cursos Matriculados</div>
+                <div>
+                  <Table striped bordered hover size="sm">
+                  <thead>
+                  <tr>
+                    <th>Curso</th>
+                    <th>Código</th>
+                    <th>Grupo</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  <tr>
+                    <td>Taller de Digital</td>
+                    <td>293-CE</td>
+                    <td>03</td>
+                  </tr>
+                  <tr>
+                    <td>Arquitectura de Computadores 1</td>
+                    <td>394-CE</td>
+                    <td>01</td>
+                  </tr>
+                  <tr>
+                    <td>Probabilidad y Estadística</td>
+                    <td>404-EL</td>
+                    <td>01</td>
+                  </tr>
+                  <tr>
+                    <td>Física 04</td>
+                    <td>302-FI</td>
+                    <td>05</td>
+                  </tr>
+                  <tr>
+                    <td>Especificación y Diseño de Software</td>
+                    <td>494-CE</td>
+                    <td>02</td>
+                  </tr>
+                  </tbody>
+                </Table>
+                </div>
+              </Col>
+            </Row>
+            <Row>
+              <Col md={{ span: 6}}>
+                <div>
+                  <Container>
+                    <Row>
+                      <Col>
+                        Información de Contacto
+                      </Col>
+                      <Col>
+                        <Form>
+                          <Form.Group controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Editar" />
+                          </Form.Group>
+                        </Form>
+                      </Col>
+                    </Row>
+                  </Container>
+                </div>
+                <div>
+                  <Container>
+                    <Form>
+                      <Form.Group as={Row} controlId="formPlaintextEmail">
+                        <Form.Label column sm="4">
+                          Email 1:
+                        </Form.Label>
+                        <Col sm="8">
+                          <Form.Control plaintext readOnly defaultValue="lhemsworth@gmail.com" />
+                        </Col>
+                      </Form.Group>
+                      <Form.Group as={Row} controlId="formPlaintextEmail">
+                        <Form.Label column sm="4">
+                          Email 2:
+                        </Form.Label>
+                        <Col sm="8">
+                          <Form.Control plaintext readOnly defaultValue="lhemsworth@xtec.com" />
+                        </Col>
+                      </Form.Group>
+                      <Form.Group as={Row} controlId="formPlaintextEmail">
+                        <Form.Label column sm="4">
+                          Teléfono
+                        </Form.Label>
+                        <Col sm="8">
+                          <Form.Control plaintext readOnly defaultValue="+506 79126670" />
+                        </Col>
+                      </Form.Group>
+                    </Form>
+                  </Container>
+                </div>
+              </Col>
+              <Col md={{ span: 6}}>
+                <div>
+                  TEC-Colones
+                </div>
+                <div>
+                  <Form>
+                    <Form.Group as={Row} controlId="formPlaintextEmail">
+                      <Form.Label column sm="2">
+                        $TEC
+                      </Form.Label>
+                      <Col sm="10">
+                        <Form.Control plaintext readOnly defaultValue="10000" />
+                      </Col>
+                    </Form.Group>
+                  </Form>
+                </div>
+              </Col>
+            </Row>
+
           </Container>
 
         </Container>
